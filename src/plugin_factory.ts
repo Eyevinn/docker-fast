dc2671c585743b1edd372ecea2e8d6749b26c9ad
Jonas Birmé <jonas.birme@eyevinn.se>
2023-06-09 11:00:40 +0200

    feat: plugin to use a custom nextVod webhook
    With the WebHook plugin you can use a custom webhook (HTTP callback) on deciding what the
    engine should play next for a channel.

--- src/plugin_factory.ts
+++ src/plugin_factory.ts
@@ -4,6 +4,7 @@ import { ScheduleServicePlugin } from './plugins/plugin_schedule_service';
 import { LoopPlugin } from './plugins/plugin_loop';
 import { PlaylistPlugin } from './plugins/plugin_playlist';
 import { BarkerPlugin } from './plugins/plugin_barker';
+import { WebHookPlugin } from './plugins/plugin_webhook';
 
 function create<T extends PluginInterface>(c: { new (): T }): T {
   return new c();
@@ -21,6 +22,8 @@ export function PluginFactory(pluginName: string) {
       return create(PlaylistPlugin);
     case 'Barker':
       return create(BarkerPlugin);
+    case 'WebHook':
+      return create(WebHookPlugin);
     default:
       throw new Error(`Plugin ${pluginName} is not available`);
   }
