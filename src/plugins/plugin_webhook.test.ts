62fed4f70f70ee6f9415a2e7b3c937e883cabf3e
Jonas Birmé <jonas.birme@eyevinn.se>
2023-12-11 11:13:06 +0100

    feat: option to provide webhook with api key

--- src/plugins/plugin_webhook.test.ts
+++ src/plugins/plugin_webhook.test.ts
@@ -2,6 +2,17 @@ import { WebHookPlugin } from './plugin_webhook';
 import fetchMock from 'jest-fetch-mock';
 
 describe('webhook plugin', () => {
+  const OLD_ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...OLD_ENV };
+  });
+
+  afterAll(() => {
+    process.env = OLD_ENV;
+  });
+
   test('provides channel id only once on multiple requests', async () => {
     fetchMock.mockResponse(
       JSON.stringify({
@@ -29,5 +40,54 @@ describe('webhook plugin', () => {
     expect(fetchMock.mock.calls[1][0]).toEqual(
       'http://localhost:8002/nextVod?channelId=test'
     );
+  });
+
+  test('provides apikey on http request when enabled', async () => {
+    process.env.OPTS_WEBHOOK_APIKEY = 'myapikey';
+
+    fetchMock.mockResponse(
+      JSON.stringify({
+        hlsUrl: 'https://vod.dummy/foo.m3u8',
+        id: 'dummy',
+        title: 'dummytitle'
+      })
+    );
+
+    const plugin = new WebHookPlugin();
+    const assetManager = plugin.newAssetManager();
+    await assetManager.getNextVod({
+      sessionId: 'dummy',
+      playlistId: 'test'
+    });
+    expect(fetchMock.mock.calls[0][1]?.headers).toBeDefined();
+    if (fetchMock.mock.calls[0][1]?.headers) {
+      expect(fetchMock.mock.calls[0][1].headers['Authorization']).toEqual(
+        'Bearer myapikey'
+      );
+    }
+  });
+
+  test('does not provides apikey on http request if not enabled', async () => {
+    process.env.OPTS_WEBHOOK_APIKEY = undefined;
+
+    fetchMock.mockResponse(
+      JSON.stringify({
+        hlsUrl: 'https://vod.dummy/foo.m3u8',
+        id: 'dummy',
+        title: 'dummytitle'
+      })
+    );
+
+    const plugin = new WebHookPlugin();
+    const assetManager = plugin.newAssetManager();
+    await assetManager.getNextVod({
+      sessionId: 'dummy',
+      playlistId: 'test'
+    });
+    if (fetchMock.mock.calls[0][1]?.headers) {
+      expect(
+        fetchMock.mock.calls[0][1].headers['Authorization']
+      ).toBeUndefined();
+    }
   });
 });
